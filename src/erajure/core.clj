74773bdcc2567e633f7adc452e40f7e6d15a007e
David Chelimsky <dchelimsky@drwholdings.com>
2012-10-19 11:41:58 -0500

    fully qualify Mockito to save upstream import requirement

--- src/erajure/core.clj
+++ src/erajure/core.clj
@@ -1,24 +1,24 @@
 (ns erajure.core
-  (:import [org.mockito Mockito]))
+  (:import [org.mockito]))
 
 (defn mock
   ([klass]
-    (Mockito/mock klass))
+    (org.mockito.Mockito/mock klass))
   ([klass behavior]
-    (doto (Mockito/mock klass)
+    (doto (org.mockito.Mockito/mock klass)
       behavior)))
 
 (defmacro verify
   ([klass form]
-    `(-> (Mockito/verify ~klass) ~form))
+    `(-> (org.mockito.Mockito/verify ~klass) ~form))
   ([klass times form]
-    `(-> (Mockito/verify ~klass ~times) ~form)))
+    `(-> (org.mockito.Mockito/verify ~klass ~times) ~form)))
 
 (defn ->when [[meth r]]
-  (list '-> 'm meth 'Mockito/when (list '.thenReturn r)))
+  (list '-> 'm meth 'org.mockito.Mockito/when (list '.thenReturn r)))
 
 (defmacro behavior [& args]
   `(fn ~'[m]
     ~@(map ->when (partition 2 args))))
 
-(def never (Mockito/never))
+(def never (org.mockito.Mockito/never))
