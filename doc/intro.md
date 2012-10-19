3854abe4b182faf7c0ef54cf2d37c52c5b4e5a13
Jay Fields <github@jayfields.com>
2012-10-19 11:15:53 -0400

    lein enw

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to erajure
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/erajure/core_test.clj
@@ -0,0 +1,7 @@
+(ns erajure.core-test
+  (:use clojure.test
+        erajure.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
